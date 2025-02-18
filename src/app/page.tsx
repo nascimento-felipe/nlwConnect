5d6eb8c49d9dd5d957d737df03fb9e50d91e3068
Felipe <61094586+nascimento-felipe@users.noreply.github.com>
2025-02-18 12:28:06 -0300

    feat: :sparkles: Estilos dos botoes feitos, parte do input feito

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
+import { Button } from "@/components/button";
+import { IconButton } from "@/components/icon-button";
+import { Input } from "@/components/input";
+import { ArrowRight, Copy } from "lucide-react";
+
 export default function Home() {
   return (
     <main>
-      <div>Hello world!</div>
+      <Button>
+        Enviar
+        <ArrowRight />
+      </Button>
+
+      <IconButton>
+        <Copy />
+      </IconButton>
+
+      <div>
+        <Input placeholder="Digite seu email" />
+      </div>
     </main>
   );
 }
