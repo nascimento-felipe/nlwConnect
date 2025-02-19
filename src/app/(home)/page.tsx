ae45bcd06f9eb3e88f22b56e046e74572d117b5b
Felipe <61094586+nascimento-felipe@users.noreply.github.com>
2025-02-19 12:46:56 -0300

    feat: :sparkles: Form com validacao e funcionalidade do botao de copiar

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,8 @@
 import Image from "next/image";
 
-import { Button } from "@/components/button";
-import { InputField, InputIcon, InputRoot } from "@/components/input";
-import { ArrowRight, Radio, User } from "lucide-react";
-import logo from "../assets/logo.svg";
+import { Radio } from "lucide-react";
+import logo from "../../assets/logo.svg";
+import { SubscriptionForm } from "./subscription-form";
 
 export default function Home() {
   return (
@@ -39,35 +38,8 @@ export default function Home() {
             Dias 15 a 17 de março | Das 18h às 21h | Online & Gratuito
           </p>
         </div>
-        <form
-          className="bg-gray-700 border border-gray-600 rounded-2xl p-8 space-y-6 w-full md:max-w-[440px]"
-          action=""
-        >
-          <h2 className="font-heading font-semibold text-gray-200 text-xl">
-            Inscrição
-          </h2>
 
-          <div className="space-y-3">
-            <InputRoot>
-              <InputIcon>
-                <User />
-              </InputIcon>
-              <InputField type="text" placeholder="Nome completo" />
-            </InputRoot>
-
-            <InputRoot>
-              <InputIcon>
-                <User />
-              </InputIcon>
-              <InputField type="email" placeholder="E-mail" />
-            </InputRoot>
-          </div>
-
-          <Button type="submit">
-            Confirmar
-            <ArrowRight />
-          </Button>
-        </form>
+        <SubscriptionForm />
       </div>
     </div>
   );
--- src/app/invite/invite-link-input.tsx
+++ src/app/invite/invite-link-input.tsx
@@ -1,21 +1,28 @@
+"use client";
+
 import { IconButton } from "@/components/icon-button";
 import { InputField, InputIcon, InputRoot } from "@/components/input";
 import { Copy, Link } from "lucide-react";
 
-export function InviteLinkInput() {
+interface InviteInputLinkProps {
+  inviteLink: string;
+}
+
+export function InviteLinkInput({ inviteLink }: InviteInputLinkProps) {
+  function copyInviteLink() {
+    navigator.clipboard.writeText(inviteLink);
+  }
+
   return (
     <InputRoot>
       <InputIcon>
         <Link className="size-5" />
       </InputIcon>
 
-      <InputField
-        readOnly
-        defaultValue={"http://localhost:3000/invite/19827381772"}
-      />
+      <InputField readOnly defaultValue={inviteLink} />
 
       <IconButton className="-mr-2">
-        <Copy className="size-5" />
+        <Copy className="size-5" onClick={copyInviteLink} />
       </IconButton>
     </InputRoot>
   );
